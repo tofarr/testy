8e72fd1ce255f1bd80b67918b98cbedfe3702e08
Tim O'Farrell <tofarr@everyonesocial.com>
2019-10-23 21:22:54 -0600

    Custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,23 @@ import './App.css';
 import { BrowserRouter as Router } from "react-router-dom";
 import { Box, Container, Paper } from '@material-ui/core';
 
+import CustomTheme from './components/CustomTheme';
 import Header from './components/Header';
 import AppController from './AppController';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Container>
-        <Header />
-        <Paper>
-          <Box p={2} mt={10}>
-            <AppController />
-          </Box>
-        </Paper>
-      </Container>
+      <CustomTheme>
+        <Container>
+          <Header />
+          <Paper>
+            <Box p={2} mt={10}>
+              <AppController />
+            </Box>
+          </Paper>
+        </Container>
+      </CustomTheme>
     </Router>
   );
 }
---
+++ src/components/CustomTheme.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+  }
+});
+
+const CustomTheme: React.FC = ({ children }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      {children}
+    </ThemeProvider>
+  );
+}
+
+export default CustomTheme;
